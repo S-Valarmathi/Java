2c7263d08c7d6944f6488cbfefc2a101326cbe69
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-06-22 19:02:37 +0530

    feat: Added String Program

---
+++ NonPrimitive Datatypes/StringLength.java
@@ -0,0 +1,8 @@
+class StringLength{
+    public static void main(String[] args){
+        String name = "Valarmathi";
+        System.out.println("Name: " + name);
+        System.out.println("Length: " + name.length());
+
+    }
+}
