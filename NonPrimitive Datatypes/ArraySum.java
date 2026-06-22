b8963fcb1603e912bcd319fc58164b2bb329ff04
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-06-22 19:33:10 +0530

    feat: Added array sum program

---
+++ NonPrimitive Datatypes/ArraySum.java
@@ -0,0 +1,13 @@
+class ArraySum{
+    public static void main(String[] args){
+        int[] nums = {10, 20, 30, 40, 50};
+        int sum = 0;
+
+        for(int i = 0; i < nums.length; i++ ){
+            sum = sum + nums[i];
+        }
+
+        System.out.println("Sum of Array: " + sum );
+
+    }
+}
