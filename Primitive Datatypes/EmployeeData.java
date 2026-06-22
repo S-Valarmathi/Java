ef9836291768545a5dbf382d559132e75790e953
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-06-22 13:56:57 +0530

    feat: implement employee data program

---
+++ Primitive Datatypes/EmployeeData.java
@@ -0,0 +1,24 @@
+class EmployeeData {
+    public static void main(String[] args) {
+
+        String name = "aaaaaaa";
+        byte age = 24;
+        short exp = 2;
+        int id = 1002;
+        long phone = 9876543211L;
+        float rating = 4.8f;
+        double salary = 50000.75;
+        char grade = 'A';
+        boolean status = true;
+
+        System.out.println("Employee Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Experience: " + exp + " years");
+        System.out.println("Employee ID: " + id);
+        System.out.println("Phone Number: " + phone);
+        System.out.println("Performance Rating: " + rating);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Working Status: " + status);
+    }
+}
