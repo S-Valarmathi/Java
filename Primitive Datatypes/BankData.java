1945d6f3c625d0f7bd5f3928c5b2bba47654f1f6
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-06-22 13:50:18 +0530

    feat: implement bank data program

---
+++ Primitive Datatypes/BankData.java
@@ -0,0 +1,23 @@
+class BankData{
+    public static void main(String[] args){
+        String name = "Valarmathi";
+        byte age = 19;
+        short code = 1201;
+        int AccNo = 567890;
+        long CustomerNo = 9876543210L;
+        float Interest = 6.5f;
+        double balance = 25000.75;
+        char type = 'S';
+        boolean active = true;
+        System.out.println( "Account Holder Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Branch Code: " + code);
+        System.out.println("Account Number: " + AccNo);
+        System.out.println("Customer Number: " + CustomerNo);
+        System.out.println("Interest Rate: " + Interest);
+        System.out.println("Balance: " + balance);
+        System.out.println("Account Type: " + type);
+        System.out.println("Active Status: " + active);
+
+    }
+}
