a28652c815ed6a8ea26bafb48a5e867923bb9aec
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-06-22 13:54:15 +0530

    feat: implement product details program

---
+++ Primitive Datatypes/ProductDetails.java
@@ -0,0 +1,13 @@
+class ProductDetails{
+    public static void main(String[] args){
+        String a = "Laptop";
+        int b = 101;
+        double c = 55000.00;
+        boolean d = true;
+        System.out.println("ProductName: " + a);
+        System.out.println("Product ID: " + b);
+        System.out.println("Price: " + c);
+        System.out.println("Available: " + d);
+        
+    }
+}
