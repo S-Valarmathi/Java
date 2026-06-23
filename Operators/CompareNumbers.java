beb5cead5713560d7f68b89e404ee49a8ed6c481
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-06-23 20:21:15 +0530

    feat: add number comparison using relational operator

---
+++ Operators/CompareNumbers.java
@@ -0,0 +1,15 @@
+package Operators;
+
+public class CompareNumbers {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        if (a == b){
+            System.out.println("Numbers are equal");
+        }
+        else{
+            System.out.println("Numbers are not equal");
+        }
+    }
+    
+}
