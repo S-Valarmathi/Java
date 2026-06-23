9bb6bd2c9a174fa0f176b67dbb8e551fd50debe8
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-06-23 21:48:48 +0530

    feat: add account status using logical not operator

---
+++ Operators/AccountStatus.java
@@ -0,0 +1,13 @@
+package Operators;
+
+public class AccountStatus{
+    public static void main(String[] args){
+        boolean isBlocked = false;
+        if(!isBlocked){
+            System.out.println("Account is Active ");
+        }
+        else{
+            System.out.println("Account is Blocked");
+        }
+    }
+}
