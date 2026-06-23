7b185fca1c2a4c539ee47b2356ade7e7ed2d041c
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-06-23 22:21:32 +0530

    feat: add bank balance update using assignment operator

---
+++ Operators/BankBalance.java
@@ -0,0 +1,13 @@
+package Operators;
+
+public class BankBalance {
+    public static void main(String[] args){
+        int balance = 12000;
+        balance += 5000;
+        balance -= 2000;
+
+        System.out.println("Final Balance: " + balance);
+
+    }
+    
+}
