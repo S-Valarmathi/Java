52e141c6b40134cd360cabbe19de16752668809d
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-06-23 20:26:19 +0530

    feat: add biggest number using relational operator

---
+++ Operators/BiggestNumber.java
@@ -0,0 +1,15 @@
+package Operators;
+
+public class BiggestNumber {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        if(a > b){
+            System.out.println("a is Bigger");
+        }
+        else{
+            System.out.println("b is Bigger");
+        }
+    }
+    
+}
