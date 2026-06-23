5b7db90963f5123a097664e78bba0d178b6c5e98
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-06-23 20:32:45 +0530

    add positive negative check using relational operator

---
+++ Operators/PositiveNegative.java
@@ -0,0 +1,17 @@
+package Operators;
+
+public class PositiveNegative {
+    public static void main(String[] args){
+        int number = 10;
+        if (number > 0){
+            System.out.println("Positive Number");
+        }
+        else if (number < 0){
+            System.out.println("Negative Number");
+        }
+        else{
+            System.out.println("Zero");
+        }
+    }
+    
+}
