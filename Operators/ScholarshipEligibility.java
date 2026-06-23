28da3690e7267ebf0ebd00c702468cbd7f743881
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-06-23 20:49:13 +0530

    feat: add scholarship  eligibility using logical or operator

---
+++ Operators/ScholarshipEligibility.java
@@ -0,0 +1,15 @@
+package Operators;
+
+public class ScholarshipEligibility {
+    public static void main(String[] args) {
+        int marks = 80;
+        boolean SportsParticipation = true;
+        if(marks >= 90 || SportsParticipation == true){
+            System.out.println("Eligible for scholarship");
+        }
+        else{
+            System.out.println("Noteligible for scholarship");
+        }
+        } 
+    }
+    
