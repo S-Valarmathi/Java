30837f30bba04562cfbd57866dccb690ac0d9436
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-06-23 20:10:09 +0530

    feat: add average calculation using arithmetic operator

---
+++ Operators/AverageCalculation.java
@@ -0,0 +1,15 @@
+package Operators;
+
+public class AverageCalculation {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int sum = a + b + c;
+        int average = sum/3;
+        System.out.println("Sum: " + sum);
+        System.out.println("Average: " + average);
+
+    }
+    
+}
