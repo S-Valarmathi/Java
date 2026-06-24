18c7567a00530790fc3978c3ee21c69e28bafd5f
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-06-24 19:49:36 +0530

    feat: add square method program

---
+++ NonPrimitive Datatypes/Methods/Square.java
@@ -0,0 +1,11 @@
+package Methods;
+
+public class Square {
+    static void square (int num){
+        System.out.println(num * num);
+    }
+    public static void main(String[] args){
+        square(4);
+    }
+    
+}
