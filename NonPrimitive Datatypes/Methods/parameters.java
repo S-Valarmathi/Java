0884f3756f8a35f7f0d7a3436ba1d8b07ee047a0
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-06-24 19:43:38 +0530

    feat: add parameter program

---
+++ NonPrimitive Datatypes/Methods/parameters.java
@@ -0,0 +1,10 @@
+package Methods;
+
+public class parameters {
+    static void greet(String name){
+        System.out.println("Hello " + name);
+    }
+    public static void main(String[] args){
+        greet("Valarmathi");
+    }
+}
