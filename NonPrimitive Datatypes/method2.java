88d14d4d098a1adc1a950c0fb28dd1b15ae055c6
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-06-24 19:33:58 +0530

    feat: add method basic program

---
+++ NonPrimitive Datatypes/method2.java
@@ -0,0 +1,13 @@
+public class method2 {
+    static void greet(){
+        System.out.println("Valarmathi");
+    }
+    public static void main(String[] args){
+        greet();
+        greet();
+        greet();
+        greet();
+        greet();
+    }
+    
+}
