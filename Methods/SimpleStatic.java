48dc21c0ac8edc7763d88f7f51f309c0bca9c65c
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-06-25 19:59:15 +0530

    feat: add simple static program

---
+++ Methods/SimpleStatic.java
@@ -0,0 +1,11 @@
+package Methods;
+
+public class SimpleStatic {
+    static void greet(){
+        System.out.println("Good Morning");
+    }
+    public static void main(String[] args){
+        greet();
+    }
+    
+}
