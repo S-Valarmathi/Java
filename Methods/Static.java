07767d4d5f9fbfe9bc9b00f9df1b2481816f3c28
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-06-25 19:57:16 +0530

    feat: add static method program

---
+++ Methods/Static.java
@@ -0,0 +1,11 @@
+package Methods;
+
+public class Static {
+    static void hello(){
+        System.out.println("Hello");
+    }
+    public static void main(String[] args){
+        hello();
+    }
+    
+}
