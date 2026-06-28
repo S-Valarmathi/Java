364593bcad613fd48f5885b93ccc8a3ade554d5d
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-06-28 11:29:20 +0530

    feat: add basic array program

---
+++ Arrays/Array.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class Array{
+    public static void main (String[] args){
+        int[]  marks = { 90, 95, 85};
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+    }
+    
+}
