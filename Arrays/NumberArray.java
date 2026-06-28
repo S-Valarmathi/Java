ae750d7743826d4745a88995997b56f648d4a929
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-06-28 12:58:22 +0530

    feat: add two dimensional array program

---
+++ Arrays/NumberArray.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class NumberArray {
+    public static void main(String[] args){
+        int[][] arr = {
+            {10,20},
+            {30,40}
+        };
+        System.out.println(arr[0][0]);
+        System.out.println(arr[0][1]);
+        System.out.println(arr[1][0]);
+        System.out.println(arr[1][1]);
+
+
+
+    }
+    
+}
