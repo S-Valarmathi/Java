b933d1b69f12c73f0a8bfd23af24b17c5ebe62e5
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-06-29 14:12:41 +0530

    feat: add array traversal program

---
+++ Arrays/ArrayTraversal.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+public class ArrayTraversal {
+    public static void main(String[] args){
+        int[] numbers = {5, 10, 15, 20, 25};
+        for(int value : numbers){
+            System.out.println(value);
+        }
+
+        
+    }
+    
+}
