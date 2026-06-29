b8fec7d4d816e2a22f40737397b43d477e76bee2
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-06-29 20:08:53 +0530

    feat: add to upper case program

---
+++ Strings/ToUpperCase.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class ToUpperCase {
+    public static void main(String[] args){
+        String word = "Valarmathi";
+        System.out.println(word.toUpperCase());
+    }
+    
+}
