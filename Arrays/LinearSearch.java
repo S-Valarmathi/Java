9e9983979e3b679658112b629a0e43c3df470210
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-06-29 14:25:06 +0530

    feat: add linear search program

---
+++ Arrays/LinearSearch.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public class LinearSearch {
+    public static void main(String[] args){
+        int[] numbers = {10,20,30,40,50};
+        int search = 30;
+        boolean found = false;
+
+        for(int i=0; i<numbers.length; i++){
+            if(numbers[i] == search){
+                found = true;
+                break;
+            }
+        }
+
+        if(found)
+            System.out.println("Element Found");
+        else
+            System.out.println("Element Not Found");
+    }
+    
+}
