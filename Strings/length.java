6575f38f4c0c3c0913a6d60f41e4de92a929e188
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-06-29 20:01:57 +0530

    feat: add string length program

---
+++ Strings/length.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class length {
+    public static void main(String[] args){
+        String name = "Valarmathi";
+        System.out.println(name.length());
+    }
+    
+}
