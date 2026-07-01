062771429bb4a44f477d8f072d44dfa6f159d34c
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-07-01 18:58:39 +0530

    feat: add trim method program

---
+++ Strings/TrimString.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class TrimString {
+    public static void main(String[] args){
+        String name = "     Java Programming ";
+        String result = name.trim();
+        System.out.println("Before Trim: " + name);
+        System.out.println("After Trim: " + result);
+    }
+    
+}
