281c423161ea460c83acac1343d0c942f8e27276
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-07-01 18:55:13 +0530

    feat: add replace method program

---
+++ Strings/ReplaceString.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class ReplaceString {
+    public static void main(String[] args){
+        String name ="Java";
+        String result = name.replace('a','o');
+        System.out.println("Updated String: " + result);
+    }
+    
+}
