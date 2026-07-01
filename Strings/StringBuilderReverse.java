302a28f5a88331cd2733a5c0407efec1ac2dde54
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-07-01 19:39:36 +0530

    feat: add stringbuilder reverse program

---
+++ Strings/StringBuilderReverse.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class StringBuilderReverse {
+    public static void main(String[] args){
+        StringBuilder sb = new StringBuilder("Java");
+        sb.reverse();
+        System.out.println(sb);
+    }
+    
+}
