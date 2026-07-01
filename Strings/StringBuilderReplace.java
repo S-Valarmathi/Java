7a832a64e82a39aa19e11f6393da243f37ad18fc
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-07-01 19:29:34 +0530

    feat: add stringbuilder replace program

---
+++ Strings/StringBuilderReplace.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class StringBuilderReplace {
+    public static void main(String[] args){
+        StringBuilder sb = new StringBuilder("Java");
+        sb.replace(0, 3, "Python");
+    }
+    
+}
