9d971029dd413d56ee22512279381f05c767b73c
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-07-01 18:47:12 +0530

    feat: add substring method program

---
+++ Strings/SubString.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class SubString {
+    public static void main(String[] args){
+        String name = "Java Programming";
+        String result = name.substring (4, 16);
+        System.out.println("Substring: " + result);
+
+    }
+    
+}
