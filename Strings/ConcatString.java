a5c6f0510aa664095377d4f62e93df302162c6b0
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-07-01 19:03:31 +0530

    feat: add concat method program

---
+++ Strings/ConcatString.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class ConcatString {
+    public static void main(String[] args){
+        String name = "Valar";
+        String result = name.concat("mathi");
+        System.out.println("Combined String: " + result);
+    }
+    
+}
