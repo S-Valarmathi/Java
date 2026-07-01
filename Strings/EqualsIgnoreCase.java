c9fc344ce6eb8528d6f4a83564db1f7d70e31bdd
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-07-01 18:42:46 +0530

    feat: add equals ignore case string program

---
+++ Strings/EqualsIgnoreCase.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class EqualsIgnoreCase {
+    public static void main(String[] args){
+        String first = "java";
+        String second = "JAVA";
+        boolean result = first.equalsIgnoreCase(second);
+        System.out.println("String are equals: " + result);
+    }
+    
+}
