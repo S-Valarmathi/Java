79e8bd1e1189fac808a192a4d06e55f42882264f
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-07-01 18:38:25 +0530

    feat: add string equals method program

---
+++ Strings/EqualsMethod.java
@@ -0,0 +1,12 @@
+package Strings;
+
+public class EqualsMethod {
+    public static void main(String[] args){
+        String First = "Java";
+        String Second = "Java";
+        boolean result = First.equals(Second);
+        System.out.println("Strings are Equals: " + result) ;
+
+    }
+    
+}
