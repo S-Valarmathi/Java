e16939baed5b22e05aeff080360046a97f8403b7
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-07-01 19:20:48 +0530

    feat: add stringbuilder create program

--- Strings/StringBuilderCreate.java
+++ Strings/StringBuilderCreate.java
@@ -1,6 +1,6 @@
 package Strings;
 
-public class StringBuilderInsert {
+public class StringBuilderCreate {
     public static void main(String[] args){
         StringBuilder sb = new StringBuilder("Java");
         System.out.println(sb);
