7077e91e97f5dfcbaa31c222079b4095d667f639
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-07-01 19:16:03 +0530

    feat: add stringbuilder insert program

---
+++ Strings/StringBuilderInsert.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class StringBuilderInsert {
+    public static void main(String[] args){
+        StringBuilder sb = new StringBuilder("Java");
+        System.out.println(sb);
+    }
+    
+}
