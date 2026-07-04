e25d788542de025acc593197f5b6f4ce760f6296
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-07-04 19:17:29 +0530

    feat: add parameterized constructor for restaurant

---
+++ OOP/Restaurant.java
@@ -0,0 +1,30 @@
+package OOP;
+
+public class Restaurant {
+    String restaurantName;
+    String location;
+    double rating;
+
+    Restaurant(String a, String b, double c){
+        restaurantName = a;
+        location = b;
+        rating = c;
+    }
+
+    void display(){
+        System.out.println("Restaurant Name: " + restaurantName);
+        System.out.println("Location: " + location);
+        System.out.println("Rating: " + rating);
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+        Restaurant r1 = new Restaurant("A2B", "Coimbatore", 4.5);
+        Restaurant r2 = new Restaurant("SS Hyderabad Biryani",  "Chennai", 4.7 );
+        r1.display();
+        r2.display();
+
+    }
+
+    
+}
