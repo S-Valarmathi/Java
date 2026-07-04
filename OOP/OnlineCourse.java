36fd9b922ae8a7e4e1c16db1da80bff0f2034aa0
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-07-04 20:10:44 +0530

    feat: use this keyword in online course class

---
+++ OOP/OnlineCourse.java
@@ -0,0 +1,33 @@
+package OOP;
+
+public class OnlineCourse {
+    String CourseName;
+    String TrainerName;
+    int duration;
+    int fees;
+
+    OnlineCourse(String CourseName, String TrainerName, int duration, int fees){
+        this.CourseName = CourseName;
+        this.TrainerName = TrainerName;
+        this.duration = duration;
+        this.fees = fees;
+
+    }
+
+    void display(){
+        System.out.println("Course Name: " + CourseName);
+        System.out.println("Trainer Name: " + TrainerName);
+        System.out.println("Duration: " + duration);
+        System.out.println("Fees: " + fees);
+        System.out.println();
+
+    }
+
+    public static void main(String[] args){
+        OnlineCourse c1 = new OnlineCourse("Data Science", "Priya", 8, 3500 );
+        OnlineCourse c2 = new OnlineCourse("Cloud Computing", "dharshini", 12, 35000);
+        c1.display();
+        c2.display();
+    }
+    
+}
