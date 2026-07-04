0517f83642324619ab61acb0e0b2cd345f8d0aa3
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-07-04 19:04:57 +0530

    feat: add parameterized constructor for student details

---
+++ OOP/StudentDetails.java
@@ -0,0 +1,21 @@
+package OOP;
+
+public class StudentDetails{
+    String name;
+    int age;
+
+    StudentDetails(String n, int a ){
+        name = n;
+        age = a;
+    }
+
+    void display(){
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+    public static void main(String[] args){
+        StudentDetails s1 = new StudentDetails ("Valarmathi", 19);
+        s1.display();
+    }
+    
+}
