ce528af2d3a7d46e33c0a2b15c5e84f1e1f6ae82
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-07-04 11:46:36 +0530

    feat: create employee class with two object

---
+++ OOP/Employee.java
@@ -0,0 +1,34 @@
+package OOP;
+
+public class Employee {
+    int empid;
+    String empName;
+    String department;
+
+    void display(){
+        System.out.println("EmpId: " + empid);
+        System.out.println("EmpName: " + empName);
+        System.out.println("Department: " + department);
+
+    }
+
+    public static void main(String[] args){
+        Employee e1 = new Employee();
+        Employee e2 = new Employee();
+
+        e1.empid = 101;
+        e1.empName = "aaa";
+        e1.department = "IT";
+
+
+        e2.empid = 102;
+        e2.empName = "bbb";
+        e2.department = "CSE";
+
+
+        e1.display();
+        e2.display();
+    } 
+
+    
+}
