8f4fef50dae0177587caee9cc70debafecb60210
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-07-06 11:46:10 +0530

    feat: implement inheritance using vehicle and bike classes

---
+++ OOP/Vehicle.java
@@ -0,0 +1,17 @@
+package OOP;
+
+public class Vehicle {
+    void start(){
+        System.out.println("Vehicle is starting");
+    }
+
+    class Bike extends Vehicle{
+
+    }
+
+    public static void main(String[] args){
+        Vehicle v1 = new Vehicle();
+        v1.start();
+    }
+    
+}
