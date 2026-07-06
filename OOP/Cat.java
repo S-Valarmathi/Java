8501a2ec6b3522ab248e1c00ba08ca251cb1822e
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-07-06 12:15:29 +0530

    feat: add cat inheritance examplem

---
+++ OOP/Cat.java
@@ -0,0 +1,22 @@
+package OOP;
+
+public class Cat {
+    void eat(){
+        System.out.println("Animal is eating");
+    } 
+
+    static  class dog extends Cat{
+        void meow(){
+        System.out.println("Cat says Meow");
+        }
+    }
+
+    public static void main(String[] args) {
+        dog  a1 = new dog();
+
+       a1.eat();
+       a1.meow();
+    }
+    
+    
+}
