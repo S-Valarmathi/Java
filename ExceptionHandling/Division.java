5e36b4b11c5f7ee019195250688c79ab98b7eb0a
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-07-09 19:42:42 +0530

    feat: implement division program using try catch

---
+++ ExceptionHandling/Division.java
@@ -0,0 +1,16 @@
+package ExceptionHandling;
+
+public class Division {
+    public static void main(String[] args){
+        int a = 10;
+        int b= 0;
+
+        try{
+
+            System.out.println(a/b);
+        } catch(ArithmeticException e){
+            System.out.println("Cannot divide by zero");
+        }
+    }
+}
+    
