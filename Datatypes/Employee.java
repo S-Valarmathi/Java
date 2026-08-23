2c6ded09d2fcc0df0ebbb7f70fa3afcfccf2cf26
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-08-23 21:59:23 +0530

    feat: Add java data type practice problem

---
+++ Datatypes/Employee.java
@@ -0,0 +1,17 @@
+package Datatypes;
+
+public class Employee {
+    public static void main(String[] args){
+        int EmployeeID = 101;
+        int salary = 25000;
+        char nameInitial = 'S';
+        boolean isEmployeeActive = true;
+
+        System.out.println(EmployeeID);
+        System.out.println(salary);
+        System.out.println(nameInitial);
+        System.out.println(isEmployeeActive);
+
+    }
+    
+}
