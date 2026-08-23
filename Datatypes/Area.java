d81d895d58a7b09320b89e21cf609bdc9180c9ca
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-08-23 22:03:47 +0530

    feat: Add java data type program

---
+++ Datatypes/Area.java
@@ -0,0 +1,12 @@
+package Datatypes;
+
+public class Area {
+    public static void main(String[] args){
+        double radius = 5.5;
+        double pi  = 3.14;
+        double area = pi*radius*radius;
+
+        System.out.println(area);
+    }
+    
+}
