d994b2bc42532c3f5d30b6da74ba171fde46df0c
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-08-23 21:45:16 +0530

    feat: Learn and practice java data tyepes

---
+++ Datatypes/DataTypeDemo.java
@@ -0,0 +1,16 @@
+package Datatypes;
+
+public class DataTypeDemo {
+    public static void main(String[] args){
+        int age = 20;
+        double mark = 80.5;
+        char grade = 'A';
+        boolean passed = true;
+
+        System.out.println(age);
+        System.out.println(mark);
+        System.out.println(grade);
+        System.out.println(passed);
+    }
+    
+}
