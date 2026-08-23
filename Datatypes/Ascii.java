f4e3ca1e9637e6ea37d199d64de544bc97b57064
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-08-23 22:08:05 +0530

    feat: Add java data type program

---
+++ Datatypes/Ascii.java
@@ -0,0 +1,12 @@
+package Datatypes;
+
+public class Ascii {
+    public static void main(String[] args){
+        char ch = 'S';
+        int ascii = ch;
+        System.out.println(ascii);
+
+
+    }
+    
+}
