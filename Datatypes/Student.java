9a6f02347c3142bf1341e9d3fd5003b1af3b02c8
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-08-23 21:52:10 +0530

    feat: Add java data type practice problem

---
+++ Datatypes/Student.java
@@ -0,0 +1,18 @@
+package Datatypes;
+
+public class Student {
+    public static void main(String[] args){
+        String name = "valarmathi";
+        int age = 19;
+        double mark = 90.5;
+        char grade = 'A';
+        boolean passed = true;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(mark);
+        System.out.println(grade);
+        System.out.println(passed);
+    }
+    
+}
