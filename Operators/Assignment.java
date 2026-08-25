fa6e5bd94e3737d8211fa5bd11c346f224fd5ae2
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-08-25 23:12:59 +0530

    feat: practice Java assignment operators

---
+++ Operators/Assignment.java
@@ -0,0 +1,19 @@
+package Operators;
+
+public class Assignment {
+    public static void main(String[] args){
+    int a = 50;
+    System.out.println("Initial = " + a);
+    a+=10;
+    System.out.println("After += 10 = " + a);
+    a-=5;
+    System.out.println("After -= 5 = " + a);
+    a*=2;
+    System.out.println("After *= 2 = " + a);
+    a/=5;
+    System.out.println("After /= 5 = " + a);
+    a%=7;
+    System.out.println("After %= 7 = " + a);
+
+    }  
+}
