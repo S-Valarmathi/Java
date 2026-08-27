c1466058daba89ab8a5c5fc63392f07d31995bf2
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-08-27 21:10:01 +0530

    feat: practice Java arrays

---
+++ Arrays/ArrayChange.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class ArrayChange {
+    public static void main(String[] args){
+        int[] numbers = {10,20,30,40,50};
+        numbers[2] = 500;
+        System.out.println(numbers[2]);
+    }
+    
+}
