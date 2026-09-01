b161d43f781f9dd6ee16540e55f6896d7998ee02
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-09-01 22:38:12 +0530

    feat: add Java array program

---
+++ Arrays/Reverse.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class Reverse {
+    public static void main(String[] args){
+        int[] numbers = {23,56,78,90,76,54};
+        for(int i=numbers.length-1;i>=0;i--){
+            System.out.println(numbers[i]);
+        }
+    }
+    
+}
