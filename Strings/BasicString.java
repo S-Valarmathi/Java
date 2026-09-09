ab3a0f5fec405aa06380b5a2b2af34ed94e674e5
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-09-09 19:34:54 +0530

    feat: Add string programs

---
+++ Strings/BasicString.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class BasicString {
+    public static void main(String[] args){
+        String s = "Valarmathi";
+        System.out.println(s.length());
+        System.out.println(s.charAt(0));
+        System.out.println(s.equals("Hello"));
+    }
+    
+}
