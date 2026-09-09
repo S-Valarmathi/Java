6997daeb3bf55c314db7412c9a140ccb6e36d1d2
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-09-09 20:01:14 +0530

    feat: Add count characters string program

---
+++ PracticeJava/CountCharacters.java
@@ -0,0 +1,12 @@
+package PracticeJava;
+
+import java.util.Scanner;
+public class CountCharacters {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(s.length());
+        sc.close();
+    }
+    
+}
