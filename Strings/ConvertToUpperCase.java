2813f30d49c3c1928ccec53519a5c6214ffb18b9
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-09-14 14:54:03 +0530

    feat: Add convert string to uppercase program

---
+++ Strings/ConvertToUpperCase.java
@@ -0,0 +1,13 @@
+package Strings;
+import java.util.Scanner;
+public class ConvertToUpperCase {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(s.toUpperCase());
+        sc.close();
+        
+    }
+    
+    
+}
