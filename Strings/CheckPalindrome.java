434d5c42faed76c5526b3b374a1672f0a94a984b
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-09-15 11:12:49 +0530

    feat: Add palindrome string rpogram

---
+++ Strings/CheckPalindrome.java
@@ -0,0 +1,20 @@
+package Strings;
+import java.util.Scanner;
+public class CheckPalindrome {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String reverse = "";
+        for(int i=s.length()-1;i>=0;i--){
+            reverse = reverse + s.charAt(i);
+        }
+        if(s.equals(reverse)){
+            System.out.println("Palindrome");
+        } else{
+            System.out.println("Not Palindrome");
+        }
+        sc.close();
+
+    }
+    
+}
