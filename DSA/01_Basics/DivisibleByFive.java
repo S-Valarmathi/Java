442ee4d65286fe75eaa7a806a91e5cbe2b5e11a9
S-Valarmathi <valarmathi.sakthivel99@gmail.com>
2026-09-22 22:30:07 +0530

    feat: add divisible by five program

---
+++ DSA/01_Basics/DivisibleByFive.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class DivisibleByFive {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n%5==0){
+            System.out.println("Divisible by 5");
+        } else{
+            System.out.println("Not Divisible by 5");
+        }
+        sc.close();
+    }
+    
+}
+
